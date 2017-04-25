71ccda586d58c10193e631640cabce3c26e69147
alexey <bobshaftoe@yandex.ru>
2017-04-25 12:14:52 +0500

    sidebar

--- web/static/js/web/components/sidebar/Sidebar.js
+++ web/static/js/web/components/sidebar/Sidebar.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 export default (props) => (
-  <ul>
-    <li><a href="">hi</a></li>
-    <li>item</li>
-    <li>item</li>
-    <li>item</li>
-  </ul>
+  <div>
+    <aside className="menu">
+      <ul className="menu-list">
+        <li><a href="#">Cards</a></li>
+        <li><a href="#">Desks</a></li>
+        <li><a href="#">Desk builder</a></li>
+        <li><a href="#">Expansions</a></li>
+        <li><a href="#">Adventures</a></li>
+        <li><a href="#">Tables</a></li>
+        <li><a href="#">Ads</a></li>
+      </ul>
+    </aside>
+  </div>
 )
